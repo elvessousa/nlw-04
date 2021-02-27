206a6bdeb501fa36af537a9d16b11ef83a17c0b5
Elves Sousa <elves.sousa@gmail.com>
2021-02-27 19:45:35 -0300

    Validation on Surveys Controller

--- node/src/controllers/SurveysController.ts
+++ node/src/controllers/SurveysController.ts
@@ -1,18 +1,34 @@
-import {Request, Response} from 'express';
-import {getCustomRepository} from 'typeorm';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
 import SurveysRepository from '../repositories/SurveysRepository';
+import * as yup from 'yup';
+import AppError from '../errors/AppError';
 
 class SurveysController {
   async create(request: Request, response: Response) {
-    const {title, description} = request.body;
-    const surveysRepository = getCustomRepository(SurveysRepository);
-    const surveyAlreadyExists = await surveysRepository.findOne({description});
+    const { title, description } = request.body;
 
-    if (surveyAlreadyExists) {
-      return response.status(400).json({error: "User already exists!"});
+    const schema = yup.object().shape({
+      title: yup.string().required('Title is required.'),
+      description: yup.string().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err);
     }
 
-    const survey = surveysRepository.create({title, description})
+    const surveysRepository = getCustomRepository(SurveysRepository);
+    const surveyAlreadyExists = await surveysRepository.findOne({
+      description,
+    });
+
+    if (surveyAlreadyExists) {
+      return response.status(400).json({ error: 'User already exists!' });
+    }
+
+    const survey = surveysRepository.create({ title, description });
     await surveysRepository.save(survey);
 
     return response.status(201).json(survey);
